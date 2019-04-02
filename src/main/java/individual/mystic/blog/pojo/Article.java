c20035872fd6b85b0fae33911193e6a6e7500c86
mystic-cg <215104920@qq.com>
2019-04-02 21:11:43 +0800

    [processing]

---
+++ src/main/java/individual/mystic/blog/pojo/Article.java
@@ -0,0 +1,14 @@
+package individual.mystic.blog.pojo;
+
+import lombok.Data;
+import lombok.ToString;
+
+import java.io.Serializable;
+
+@Data
+@ToString
+public class Article implements Serializable {
+
+
+
+}
