c2d9f455480e9e84d3cd477833feac66fa4a485b
mystic-cg <215104920@qq.com>
2019-04-12 22:12:09 +0800

    [processing]

--- src/main/java/individual/mystic/blog/controller/UserController.java
+++ src/main/java/individual/mystic/blog/controller/UserController.java
@@ -2,6 +2,7 @@ package individual.mystic.blog.controller;
 
 import individual.mystic.blog.pojo.User;
 import individual.mystic.blog.service.UserService;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +20,11 @@ public class UserController {
     @PostMapping
     public Mono<User> save(User user) {
         return userService.save(user);
+    }
+
+    @DeleteMapping
+    public Mono<Integer> remove(User user) {
+        return userService.remove(user.getUserName());
     }
 
 }
