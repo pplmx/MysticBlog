f9f6f4e83ecf478e1880f56b811a3628f642f6ec
mystic-cg <215104920@qq.com>
2019-04-22 18:42:09 +0800

    [processing]

--- src/main/java/individual/mystic/blog/pojo/Article.java
+++ src/main/java/individual/mystic/blog/pojo/Article.java
@@ -7,4 +7,15 @@ import lombok.EqualsAndHashCode;
 @EqualsAndHashCode(callSuper = true)
 public class Article extends BaseEntity {
 
+    private Integer articleID;
+    private String articleName;
+    private String articleIP;
+    private int articlePV;
+    private int articleType;
+    private String articleContent;
+    private int articleTopShow;
+    private int articleStar;
+    private Integer userID;
+    private Integer sortArticleID;
+
 }
