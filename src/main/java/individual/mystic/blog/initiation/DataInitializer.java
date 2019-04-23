a6da36fee42b42a013aa6ac6663a2d68fa909ba2
mystic-cg <215104920@qq.com>
2019-04-23 21:16:50 +0800

    [processing]

---
+++ src/main/java/individual/mystic/blog/initiation/DataInitializer.java
@@ -0,0 +1,19 @@
+package individual.mystic.blog.initiation;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@Log4j2
+@Component
+@Profile("demo")
+public class DataInitializer implements ApplicationListener<ApplicationReadyEvent> {
+
+    @Override
+    public void onApplicationEvent(ApplicationReadyEvent event) {
+
+    }
+
+}
