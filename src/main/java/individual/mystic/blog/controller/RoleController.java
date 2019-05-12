8954a6390686b6fe00172e94322cfc49ca2e1d84
mystic-cg <215104920@qq.com>
2019-05-12 19:01:09 +0800

    [processing]

--- src/main/java/individual/mystic/blog/controller/RoleController.java
+++ src/main/java/individual/mystic/blog/controller/RoleController.java
@@ -1,14 +1,23 @@
 package individual.mystic.blog.controller;
 
 import individual.mystic.blog.dao.RoleDAO;
+import individual.mystic.blog.pojo.Role;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 import javax.annotation.Resource;
 
 @RestController
-@RequestMapping("/role")
+@RequestMapping(value = "/role", consumes = MediaType.APPLICATION_JSON_VALUE)
 public class RoleController {
     @Resource
     private RoleDAO roleDAO;
+
+    @PostMapping
+    public Mono<Role> save(Role role) {
+        return roleDAO.save(role);
+    }
 }
