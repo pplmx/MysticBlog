b4f20fffcc5cff7a23ae4339a7fd5840f3c57a20
mystic-cg <215104920@qq.com>
2019-05-19 20:00:39 +0800

    [processing]

--- src/main/java/individual/mystic/blog/pojo/Privilege.java
+++ src/main/java/individual/mystic/blog/pojo/Privilege.java
@@ -2,8 +2,13 @@ package individual.mystic.blog.pojo;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import org.springframework.data.mongodb.core.index.Indexed;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
 public class Privilege extends BaseEntity {
+
+    @Indexed(unique = true)
+    private String privilegedUrl;
+
 }
