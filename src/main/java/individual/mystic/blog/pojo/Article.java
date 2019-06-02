f6ced5ac9601ca8cdf18205241584d438b5b0521
mystic-cg <215104920@qq.com>
2019-06-02 14:16:26 +0800

    [processing]

--- src/main/java/individual/mystic/blog/pojo/Article.java
+++ src/main/java/individual/mystic/blog/pojo/Article.java
@@ -2,9 +2,11 @@ package individual.mystic.blog.pojo;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
+@Document
 public class Article extends BaseEntity {
 
     private String articleName;
--- src/main/java/individual/mystic/blog/pojo/Privilege.java
+++ src/main/java/individual/mystic/blog/pojo/Privilege.java
@@ -3,9 +3,11 @@ package individual.mystic.blog.pojo;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
+@Document
 public class Privilege extends BaseEntity {
 
     @Indexed(unique = true)
