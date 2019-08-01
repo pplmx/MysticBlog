64a6d9d7d74481212ec7e7c496ad9fc3b6e4cf28
mystic-cg <215104920@qq.com>
2019-08-01 21:44:53 +0800

    [processing]

--- src/main/java/individual/mystic/blog/controller/ArticleController.java
+++ src/main/java/individual/mystic/blog/controller/ArticleController.java
@@ -1,11 +1,17 @@
 package individual.mystic.blog.controller;
 
+import individual.mystic.blog.service.ArticleService;
 import io.swagger.annotations.Api;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 @Api("Article Controller")
 @RestController
 @RequestMapping("/article")
 public class ArticleController {
+
+    @Resource
+    private ArticleService articleService;
 }
