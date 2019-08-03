8bf167041e66ed2d36fe1fabd39fb2476ad5e90a
mystic-cg <215104920@qq.com>
2019-08-03 21:37:08 +0800

    [processing]

--- src/main/java/individual/mystic/blog/security/ApplicationSecurity.java
+++ src/main/java/individual/mystic/blog/security/ApplicationSecurity.java
@@ -1,4 +1,59 @@
 package individual.mystic.blog.security;
 
+import individual.mystic.blog.actuator.FeaturesEndpoint;
+import org.springframework.boot.actuate.autoconfigure.security.reactive.EndpointRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@EnableWebFluxSecurity
+@EnableReactiveMethodSecurity
 public class ApplicationSecurity {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity serverHttpSecurity) {
+        return serverHttpSecurity.authorizeExchange()
+                .pathMatchers("/", "/admin")
+                .hasAuthority("ROLE_ADMIN")
+                .matchers(EndpointRequest.to(FeaturesEndpoint.class))
+                .permitAll()
+                .anyExchange()
+                .permitAll()
+                .and()
+                .formLogin()
+                .and()
+                .csrf()
+                .disable()
+                .build();
+    }
+
+    @Bean
+    public MapReactiveUserDetailsService userDetailsService() {
+        UserDetails user = User
+                .withUsername("user")
+                .password(passwordEncoder().encode("password"))
+                .roles("ROLE")
+                .build();
+
+        UserDetails admin = User
+                .withUsername("admin")
+                .password(passwordEncoder().encode("password"))
+                .roles("ADMIN")
+                .build();
+
+        return new MapReactiveUserDetailsService(user, admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 }
